5f3b5cfc2b83f33da51db54a8e83f9748930032b
davbauman <dbauman@ulb.ac.be>
2018-01-01 11:08:55 +0100

    Treignes-1
    Consensus_RDA

--- Script_Consensus-RDA.R
+++ Script_Consensus-RDA.R
@@ -1,3 +1,6 @@
+## Consensus RDA ##
+# *************** #
+
 # Useful packages:
 # ****************
 
@@ -31,53 +34,59 @@ Y <- spe
 
 # coeffCompare() construit le MST des différentes "association coefficients" --> permet de 
 # décider quels coefficients on garde
+# consensusRDA() à utiliser après avoir utilisé coeffCompare() pour effectuer la consRDA
+# sur les rda des coefficients sélectionnés
 
-# consensusRDA() à utiliser après avoir utilisé coeffCompare()
-
-### Construct results object
-ndis <- 16
-ordiRes <- vector("list", length = ndis)
+### Construct results object for the rda on abundances
+ndis_abun <- 10
+ordiRes_abun <- vector("list", length = ndis_abun)
 
 #---------------------------------------------
 ### Perform the various constrained ordination
 #---------------------------------------------
 ### RDA species profile
 sp <- Y / apply(Y, 1, sum)
-ordiRes[[1]] <- rda(sp~., data = env)
+ordiRes_abun[[1]] <- rda(sp ~., data = env)
 
 ### RDA chord
-chord <- Y/sqrt(apply(Y^2,1,sum))
-ordiRes[[2]] <- rda(chord~ ., data = env)
+chord <- Y/sqrt(apply(Y^2, 1, sum))
+ordiRes_abun[[2]] <- rda(chord ~., data = env)
 
 ### RDA Hellinger
 hell <- decostand(Y, method = "hellinger")
-ordiRes[[3]] <- rda(hell~ ., data = env)
+ordiRes_abun[[3]] <- rda(hell ~., data = env)
 
 ### RDA chi2
 chisq <- decostand(Y, method = "chi.square")
-ordiRes[[4]] <- rda(chisq~ ., data = env)
+ordiRes_abun[[4]] <- rda(chisq ~., data = env)
 
 ### db-RDA Bray-Curtis
-ordiRes[[5]] <- capscale(sqrt(vegdist(Y, method = "bray"))~ ., data = env, comm = Y)
+ordiRes_abun[[5]] <- capscale(sqrt(vegdist(Y, method = "bray")) ~., data = env, comm = Y)
 
 ### db-RDA square-root Bray-Curtis
-ordiRes[[6]] <- capscale(sqrt(vegdist(Y^0.5, method = "bray"))~ ., data = env, comm = Y)
+ordiRes_abun[[6]] <- capscale(sqrt(vegdist(Y^0.5, method = "bray")) ~., data = env, comm = Y)
 
 ### db-RDA fourth-root Bray-Curtis
-ordiRes[[7]] <- capscale(sqrt(vegdist(Y^0.25, method = "bray"))~ ., data = env, comm = Y)
+ordiRes_abun[[7]] <- capscale(sqrt(vegdist(Y^0.25, method = "bray")) ~., data = env, comm = Y)
 
 ### db-RDA modified Gower log 2
-ordiRes[[8]] <- capscale(vegdist(decostand(Y, "log", logbase = 2), "altGower")~ ., data = env, 
-                        comm = Y) ### Warning message stem from log transformation of 0
+ordiRes_abun[[8]] <- capscale(vegdist(decostand(Y, "log", logbase = 2), "altGower") ~., 
+                              data = env, comm = Y) ### Warning message stem from 
+# log transformation of 0
 
 ### db-RDA modified Gower log 5
-ordiRes[[9]] <- capscale(vegdist(decostand(Y, "log", logbase = 5), "altGower")~ ., data = env,
-                        comm = Y) ### Warning message stem from log transformation of 0
+ordiRes_abun[[9]] <- capscale(vegdist(decostand(Y, "log", logbase = 5), "altGower") ~., 
+                              data = env, comm = Y) ### Warning message stem from 
+# log transformation of 0
 
 ### db-RDA modified Gower log 10
-ordiRes[[10]] <- capscale(vegdist(decostand(Y, "log", logbase = 10), "altGower")~ ., data = env,
-                          comm = Y) ### Warning message stem from log transformation of 0
+ordiRes_abun[[10]] <- capscale(vegdist(decostand(Y, "log", logbase = 10), "altGower") ~., 
+                               data = env, comm = Y) ### Warning message stem from 
+# log transformation of 0
 
+### Construct results object for the rda on the presence-absence data
+ndis_bin <- 6
+ordiRes_bin <- vector("list", length = ndis_bin)
 # Transformation of abundances in presence-absence data:
 Ybin <- Y
 for (i in 1:nrow(Y)) {
@@ -89,49 +98,117 @@ Ybin[1:3, 1:10]
 
 ### RDA species profile - binary
 spbin <- Ybin / apply(Ybin, 1, sum)
-ordiRes[[11]] <- rda(spbin ~ ., data = env)
+ordiRes_bin[[1]] <- rda(spbin ~., data = env)
 
 ### db-RDA Ochiai
 library(ade4)
 och <- dist.binary(Ybin, method = 7)
-ordiRes[[12]] <- capscale(och ~ ., data = env, comm = Ybin)
+ordiRes_bin[[2]] <- capscale(och ~., data = env, comm = Ybin)
 
 ### db-RDA Raup-Crick
-ordiRes[[13]] <- capscale(sqrt(vegdist(Y, method = "raup", binary = T))~ ., data = env, 
+ordiRes_bin[[3]] <- capscale(sqrt(vegdist(Y, method = "raup", binary = T)) ~., data = env, 
                           comm = Ybin)
 
 ### RDA chi2
 chisq <- decostand(Ybin, method = "chi.square")
-ordiRes[[14]] <- rda(chisq~ ., data = env)
+ordiRes_bin[[4]] <- rda(chisq ~., data = env)
 
 ### db-RDA Jaccard
-ordiRes[[15]] <- capscale(sqrt(vegdist(Y, method = "jaccard", binary = T))~ ., data = env, 
+ordiRes_bin[[5]] <- capscale(sqrt(vegdist(Y, method = "jaccard", binary = T)) ~., data = env, 
                           comm = Ybin)
 
 ### db-RDA Soerensen (Bray-Curtis)
-ordiRes[[16]] <- capscale(sqrt(vegdist(Y, method = "bray", binary = T))~ ., data = env, 
+ordiRes_bin[[6]] <- capscale(sqrt(vegdist(Y, method = "bray", binary = T)) ~., data = env, 
                           comm = Ybin)
 
 # Significant axes of each RDA:
-rda_test <- vector("list", ndis)
-for (i in 1:ndis) rda_test[[i]] <- anova.cca(ordiRes[[i]], by = "axis")
-nb_sign_ax <- sapply(rda_test, function (x) length(which(x$Pr <= 0.05)))
+rda_test_abun <- vector("list", ndis_abun)
+for (i in 1:ndis_abun) rda_test_abun[[i]] <- anova.cca(ordiRes_abun[[i]], by = "axis")
+nb_sign_ax_abun <- sapply(rda_test_abun, function (x) length(which(x$Pr <= 0.05)))
+
+rda_test_bin <- vector("list", ndis_bin)
+for (i in 1:ndis_bin) rda_test_bin[[i]] <- anova.cca(ordiRes_bin[[i]], by = "axis")
+nb_sign_ax_bin <- sapply(rda_test_bin, function (x) length(which(x$Pr <= 0.05)))
 
 ### Compare association coefficients
-AssoComp <- coeffCompare(ordiRes, nb_sign_ax)
+AssoComp_abun <- coeffCompare(ordiRes_abun, nb_sign_ax_abun)
+AssoComp_bin  <- coeffCompare(ordiRes_bin, nb_sign_ax_bin)
 
 #---------------------------------------------
 ### Draw a graphic to visualize the comparison
 #---------------------------------------------
 ### Name of association coefficient compared
-name<-c("Species profiles", "Chord", "Hellinger", "Chi2", "Bray-Curtis", "(Bray-Curtis)^0.5",
-        "(Bray-Curtis)^0.25", "mGowerlog2", "mGowerlog5", "mGowerlog10", 
-        "Species profiles BIN", "Ochiai", "Raup", "chi2 BIN", "Jaccard", "Soerensen BIN")
+name_abun <- c("Species profiles", "Chord", "Hellinger", "Chi2", "Bray-Curtis", 
+               "(Bray-Curtis)^0.5", "(Bray-Curtis)^0.25", "mGowerlog2", "mGowerlog5", 
+               "mGowerlog10")
+name_bin <- c("Species profiles", "Ochiai", "Raup", "chi2", "Jaccard", "Soerensen")
 
-plot(AssoComp$mst, type = "t", labels = name, xlab = "", ylab = "", main = "MST Sites scores")
+plot(AssoComp_abun$mst, type = "t", labels = name, xlab = "", ylab = "", 
+     main = "MST Sites scores")
+plot(AssoComp_bin$mst, type = "t", labels = name, xlab = "", ylab = "", 
+     main = "MST Sites scores")
 
 # We remove Chi2 (abund et bin), Chord and Species profile (abund):
-ordiRes_final <- ordiRes[-c(1, 2, 4, 14)]
-(ndis <- length(ordiRes_final))
+ordiRes_abun_final <- ordiRes_abun[-c(1, 2, 4)]
+rda_test_abun_final <- rda_test_abun[-c(1, 2, 4)]
+(ndis_abun <- length(ordiRes_abun_final))
 
+ordiRes_bin_final <- ordiRes_bin[-4]
+rda_test_bin_final <- rda_test_bin[-4]
+(ndis_bin <- length(ordiRes_bin_final))
+
+source("consensusRDA_DB.R")
+consRDA_abun <- consensusRDA_DB(ordiRes_abun_final, rda_test_abun_final, Y, env, scaling = 2)
+summary(consRDA_abun)
+# How many axis are worth being considered:
+sum_eig_abun <- sum(consRDA_abun$values)
+ax_sel_abun <- which(as.numeric(consRDA_abun$values) >= mean(as.numeric(consRDA_abun$values)))
+round(consRDA_abun$values[ax_sel_abun] / sum_eig_abun, 4)
+
+axisLabels <- c(paste("Axis 1 - ", 
+                      round(consRDA_abun$values[1]/sum(consRDA_abun$values), 4) * 100, 
+                      sep = ""), 
+                paste("Axis 2 - ", 
+                      round(consRDA_abun$values[2]/sum(consRDA_abun$values), 4) * 100, 
+                      sep = ""))
+
+plot(consRDA_abun$siteConsensus[, 1:2], pch = 19, xlab = axisLabels[1], ylab = axisLabels[2], 
+     las = 1)
+abline(h = 0, v = 0, lty = 3)
+
+arrows(0, 0, consRDA_abun$spConsensus[, 1] * 0.1, consRDA_abun$spConsensus[, 2] * 0.1, 
+       col = "red", length = 0.1, angle = 12)
+text(consRDA_abun$spConsensus[, 1:2] * 0.1, labels = rownames(consRDA_abun$spConsensus), 
+     col = "red")
+
+arrows(0, 0, consRDA_abun$descConsensus[, 1] * 0.6, consRDA_abun$descConsensus[, 2] * 0.6, 
+       col = "blue", length = 0.1, angle = 12)
+text(consRDA_abun$descConsensus[, 1:2] * 0.6, labels = rownames(consRDA_abun$descConsensus), 
+     col = "blue")
+
+consRDA_bin <- consensusRDA_DB(ordiRes_bin_final, rda_test_bin_final, Ybin, env, scaling = 1)
+summary(consRDA_bin)
+# How many axis are worth being considered:
+sum_eig_bin <- sum(consRDA_bin$values)
+ax_sel_bin <- which(as.numeric(consRDA_bin$values) >= mean(as.numeric(consRDA_bin$values)))
+round(consRDA_bin$values[ax_sel_bin] / sum_eig_bin, 4)
+
+axisLabels <- c(paste("Axis 1 - ", 
+                      round(consRDA_bin$values[1]/sum(consRDA_bin$values), 4) * 100, sep = ""), 
+                paste("Axis 2 - ", 
+                      round(consRDA_bin$values[2]/sum(consRDA_bin$values), 4) * 100, sep = ""))
+
+plot(consRDA_bin$siteConsensus[, 1:2], pch = 19, xlab = axisLabels[1], ylab = axisLabels[2], 
+     las = 1)
+abline(h = 0, v = 0, lty = 3)
+
+arrows(0, 0, consRDA_bin$spConsensus[, 1] * 0.6, consRDA_bin$spConsensus[, 2] * 0.6, 
+       col = "red", length = 0.1, angle = 12)
+text(consRDA_bin$spConsensus[, 1:2] * 0.6, labels = rownames(consRDA_bin$spConsensus), 
+     col = "red")
+
+arrows(0, 0, consRDA_bin$descConsensus[, 1] * 0.6, consRDA_bin$descConsensus[, 2] * 0.6, 
+       col = "blue", length = 0.1, angle = 12)
+text(consRDA_bin$descConsensus[, 1:2] * 0.6, labels = rownames(consRDA_bin$descConsensus), 
+     col = "blue")
 
